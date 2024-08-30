60fc3bc4081a16f69ed37f6f391000ea9c71282a
Josh Martin <josh.martin@visiblebody.com>
2024-08-30 17:26:11 -0400

    continue work on setting up django

--- learnforge/learnforge/settings.py
+++ learnforge/learnforge/settings.py
@@ -37,7 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'answers',
+    'assignments',
+    'comments',
+    'courses',
+    'enrollments',
+    'lessons',
+    'questions',
+    'quizzes',
+    'submissions',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +131,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "users.User"
--- learnforge/users/models.py
+++ learnforge/users/models.py
@@ -1,6 +1,20 @@
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+# # from .managers import CustomUserManager
 
 # Create your models here.
-class User(models.Model):
+class User(AbstractUser, PermissionsMixin):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
+  email = models.EmailField(_("email address"), unique=True)
+  is_staff = models.BooleanField(default=False)
+  is_active = models.BooleanField(default=True)
+  created_at = models.DateField(default=timezone.now)
+  updated_at = models.DateField(default=timezone.now)
+  REQUIRED_FIELDS = []
+
+  def __str__(self):
+    return f"{self.firstname} {self.lastname}"
